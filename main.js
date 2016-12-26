892cfa6a72cc9db942177cf137d3dd8e6f00eb79
Victor <em3888@gmail.com>
2016-12-26 14:37:42 +0800

    Config the Console.log working

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+var rxjs_1 = require("rxjs");
+var number = [1, 5, 10]; //it is not async source
+var source = rxjs_1.Observable.from(number);
+var MyObserver = (function () {
+    function MyObserver() {
+    }
+    MyObserver.prototype.next = function (value) {
+        console.log("value: " + value);
+    };
+    MyObserver.prototype.error = function (e) {
+        console.log("Error :  " + e);
+    };
+    MyObserver.prototype.complete = function () {
+        console.log("Complete");
+    };
+    return MyObserver;
+}());
+source.subscribe(new MyObserver());
+//# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
-alert("Only would be refresh from here");
+import {Observable} from "rxjs";
+
+let number =[1,5,10] //it is not async source
+let source = Observable.from(number)
+
+class MyObserver {
+
+    next(value) {
+        console.log(`value: ${value}`);
+
+    }
+
+
+    error(e) {
+        console.log(`Error :  ${e}`);
+
+    }
+
+    complete() //Not every observable can complete, maybe something can be forever
+    {
+        console.log(`Complete`);
+    }
+}
+
+source.subscribe(new MyObserver())
+
