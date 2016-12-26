3a9310db7e2efe29d616a2ee167c712ec032821c
Victor <em3888@gmail.com>
2016-12-26 17:27:46 +0800

    Create different observable , no changes on the observer

--- main.ts
+++ main.ts
@@ -1,11 +1,57 @@
 import {Observable,Observer} from "rxjs";
 
-let number =[1,5,10] //it is not async source
-let source = Observable.from(number)
+let numbers =[1,5,10] //it is not async source
+//let source = Observable.from(number)
 
-class MyObserver implements Observer<number> {
+let source = Observable.create(  //Different way to create Observable
+    observer => {
+
+
+        let index = 0;
+        let produceValue = ()=>{
+
+            observer.next(numbers[index++]);
+
+            if (index<numbers.length)
+            {
+                setTimeout(produceValue(),2000)
+            }
+            else
+            {
+                observer.complete();
+            }
+        }
+
+        produceValue(); //if this is no here, wont call again
+
+        /*for(let n of numbers)
+        {
+            if(n=== 5) //Should be before next method
+            {
+                observer.error("Something went wrong"); //never recieve error message
+            }
+            observer.next(n);
+
+        }
+        */
+
+        observer.complete(); //This control the complete method
+    }
+
+
+)
+//let's do a easy arpproach for the arrow function
+source.subscribe(
+
+    value =>console.log(`value: soemthing else = ${value}`),
+    somethingelse =>console.log(`error: ${somethingelse}`),
+    ()=> console.log(`Something else s complete `)
+
+
+);
+
+class MyObserver implements Observer<number> { //Full blown Class is useful for add additional Api and other things
     //next: (value: number)=>void;
-
     next(value) {
         console.log(`value: ${value}`);
        // console.log(`value: ${value}`);
